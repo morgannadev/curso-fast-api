d9f94e3af5ac15c238b00b1d4f1f4ec1f808ca4f
morgannadev <morgcontato@gmail.com>
2024-02-03 18:49:24 -0300

    criação de schema e exercicios da aula

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+# o status_code 200 é padrão para rotas get
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/olamundo', status_code=200, response_class=HTMLResponse)
+def olamundo():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,31 @@
 from fastapi.testclient import TestClient
+
 from fast_zero.app import app
 
+client = TestClient(app)   # Arrange
 
 # criado com a técnica AAA (Arrange, Act, Assert)
 def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app) # Arrange
+    response = client.get('/')   # Act
 
-    response = client.get('/') # Act
+    assert response.status_code == 200   # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}   # Assert
 
-    assert response.status_code == 200 # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+
+def test_olamundo_deve_retornar_200_e_ola_mundo_em_html():
+    response = client.get('/olamundo')
+
+    assert response.status_code == 200
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+    """
+    )
