16852675c4d717c91c282d09814d1f7dcf6f6cad
morgannadev <morgcontato@gmail.com>
2024-02-03 19:47:19 -0300

    adicionando rota post para criar user e testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+database = []   # lista para ser um banco de dados provisório
 
 
 # o status_code 200 é padrão para rotas get
@@ -12,6 +13,7 @@ def read_root():
     return {'message': 'Olá Mundo!'}
 
 
+# rota feita para exercício
 @app.get('/olamundo', status_code=200, response_class=HTMLResponse)
 def olamundo():
     return """
@@ -24,3 +26,15 @@ def olamundo():
       </body>
     </html>
     """
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    # ** (unpacking ou desempacotamento)
+    # é para desempacotar o json na chamada da função
+    # user é o objeto que estamos recebendo
+    # model_dump é para fazer a serialização
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,24 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from fast_zero.app import app
 
-client = TestClient(app)   # Arrange
+
+# técnica DRY: don't repeat yourself
+@pytest.fixture   # Arrange
+def client():
+    return TestClient(app)
+
 
 # criado com a técnica AAA (Arrange, Act, Assert)
-def test_root_deve_retornar_200_e_ola_mundo():
+def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')   # Act
 
     assert response.status_code == 200   # Assert
     assert response.json() == {'message': 'Olá Mundo!'}   # Assert
 
 
-def test_olamundo_deve_retornar_200_e_ola_mundo_em_html():
+def test_olamundo_deve_retornar_200_e_ola_mundo_em_html(client):
     response = client.get('/olamundo')
 
     assert response.status_code == 200
@@ -29,3 +35,21 @@ def test_olamundo_deve_retornar_200_e_ola_mundo_em_html():
     </html>
     """
     )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
